f2c9fdbea98861a937fefbd4abefeaa134093d3e
Your Name <milhampauzan@gmail.com>
2024-10-15 07:11:42 +0700

    add commit

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -8,7 +8,7 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController nameController = TextEditingController();
@@ -16,74 +16,113 @@ class _LoginPageState extends State<LoginPage> {
   bool isLoading = false;
   String errorMessage = '';
   bool isLoginMode = true;
+  final _formKey = GlobalKey<FormState>();
+
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    // Animation controller setup with a lighter configuration
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 1500),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _animation = Tween(begin: 0.98, end: 1.02).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
 
   Future<void> login() async {
+    if (!_formKey.currentState!.validate()) return;
+
     setState(() {
       isLoading = true;
       errorMessage = '';
     });
 
-    final response = await http.post(
-      Uri.parse('https://ujikom2024pplg.smkn4bogor.sch.id/0059495358/backend/public/api/login'),
-      body: {
-        'email': emailController.text,
-        'password': passwordController.text,
-      },
-    );
+    try {
+      final response = await http.post(
+        Uri.parse('https://ujikom2024pplg.smkn4bogor.sch.id/0059495358/backend/public/api/login'),
+        body: {
+          'email': emailController.text,
+          'password': passwordController.text,
+        },
+      );
 
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      final String role = data['user']['role'];
-      final String token = data['access_token'];
-      final int userId = data['user']['id'];
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        final String role = data['user']['role'];
+        final String token = data['access_token'];
+        final int userId = data['user']['id'];
 
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setString('user_role', role);
-      await prefs.setString('auth_token', token);
-      await prefs.setInt('user_id', userId);
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString('user_role', role);
+        await prefs.setString('auth_token', token);
+        await prefs.setInt('user_id', userId);
 
-      if (role == 'admin') {
-        Navigator.pushReplacementNamed(context, '/admin_dashboard');
-      } else if (role == 'user') {
-        Navigator.pushReplacementNamed(context, '/home');
+        Navigator.pushReplacementNamed(context, role == 'admin' ? '/admin_dashboard' : '/home');
+      } else {
+        setState(() {
+          errorMessage = 'Login failed. Check your credentials.';
+        });
       }
-    } else {
+    } catch (e) {
       setState(() {
-        errorMessage = 'Login failed. Please check your credentials.';
+        errorMessage = 'Error occurred. Try again later.';
+      });
+    } finally {
+      setState(() {
+        isLoading = false;
       });
     }
-
-    setState(() {
-      isLoading = false;
-    });
   }
 
   Future<void> register() async {
+    if (!_formKey.currentState!.validate()) return;
+
     setState(() {
       isLoading = true;
       errorMessage = '';
     });
 
-    final response = await http.post(
-      Uri.parse('https://ujikom2024pplg.smkn4bogor.sch.id/0059495358/backend/public/api/register'),
-      body: {
-        'name': nameController.text,
-        'email': emailController.text,
-        'password': passwordController.text,
-      },
-    );
+    try {
+      final response = await http.post(
+        Uri.parse('https://ujikom2024pplg.smkn4bogor.sch.id/0059495358/backend/public/api/register'),
+        body: {
+          'name': nameController.text,
+          'email': emailController.text,
+          'password': passwordController.text,
+        },
+      );
 
-    if (response.statusCode == 201) {
-      Navigator.pushReplacementNamed(context, '/login');
-    } else {
+      if (response.statusCode == 201) {
+        Navigator.pushReplacementNamed(context, '/login');
+      } else {
+        setState(() {
+          errorMessage = 'Registration failed. Please check your inputs.';
+        });
+      }
+    } catch (e) {
       setState(() {
-        errorMessage = 'Register failed. Please check your inputs.';
+        errorMessage = 'Error occurred. Try again later.';
+      });
+    } finally {
+      setState(() {
+        isLoading = false;
       });
     }
-
-    setState(() {
-      isLoading = false;
-    });
   }
 
   @override
@@ -101,14 +140,11 @@ class _LoginPageState extends State<LoginPage> {
       ),
       body: Stack(
         children: [
-          // Gradient Background
+          // Optimized gradient background
           Container(
             decoration: BoxDecoration(
               gradient: LinearGradient(
-                colors: [
-                  Color(0xFF446496),
-                  Color(0xFF88A5DB),
-                ],
+                colors: [Color(0xFF446496), Color(0xFF88A5DB)],
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
               ),
@@ -116,112 +152,171 @@ class _LoginPageState extends State<LoginPage> {
           ),
           Center(
             child: SingleChildScrollView(
-              child: AnimatedContainer(
-                duration: Duration(milliseconds: 600),
-                curve: Curves.easeInOut,
-                width: MediaQuery.of(context).size.width * 0.85,
-                height: isLoginMode ? 400 : 500, // Adjust height based on mode
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.9),
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      blurRadius: 15,
-                      offset: Offset(0, 5),
-                    ),
-                  ],
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // Title based on mode (Login or Register)
-                    Text(
-                      isLoginMode ? 'Login Account' : 'Create Your Account',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF446496),
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    if (!isLoginMode)
-                      TextField(
-                        controller: nameController,
-                        decoration: InputDecoration(
-                          labelText: 'Name',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Logo with lighter animation and circular shadow
+                  AnimatedBuilder(
+                    animation: _controller,
+                    builder: (context, child) {
+                      return Transform.scale(
+                        scale: _animation.value,
+                        child: Container(
+                          width: 150,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            boxShadow: [
+                              BoxShadow(
+                                color: const Color.fromARGB(255, 255, 255, 255).withOpacity(0.2),
+                                blurRadius: 10,
+                                offset: Offset(0, 5),
+                              ),
+                            ],
+                          ),
+                          child: ClipOval(
+                            child: Image.asset(
+                              'images/7309682.png',
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
-                      ),
-                    if (!isLoginMode) SizedBox(height: 20),
-                    TextField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
+                      );
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  AnimatedContainer(
+                    duration: Duration(milliseconds: 600),
+                    curve: Curves.easeInOut,
+                    width: MediaQuery.of(context).size.width * 0.85,
+                    height: isLoginMode ? 420 : 520,
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.9),
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 10,
+                          offset: Offset(0, 5),
                         ),
-                      ),
+                      ],
                     ),
-                    SizedBox(height: 20),
-                    TextField(
-                      controller: passwordController,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      ),
-                      obscureText: true,
-                    ),
-                    SizedBox(height: 20),
-                    if (errorMessage.isNotEmpty)
-                      Text(
-                        errorMessage,
-                        style: TextStyle(color: Colors.red, fontSize: 16),
-                      ),
-                    SizedBox(height: 10),
-                    isLoading
-                        ? CircularProgressIndicator()
-                        : ElevatedButton(
-                            onPressed: () {
-                              if (isLoginMode) {
-                                login();
-                              } else {
-                                register();
-                              }
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Color(0xFF446496),
-                              padding: EdgeInsets.symmetric(horizontal: 100, vertical: 15),
-                              shape: RoundedRectangleBorder(
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // Title based on mode (Login or Register)
+                          Text(
+                            isLoginMode ? 'Login Account' : 'Create Your Account',
+                            style: TextStyle(
+                              fontSize: 26,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF446496),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          if (!isLoginMode)
+                            TextFormField(
+                              controller: nameController,
+                              decoration: InputDecoration(
+                                labelText: 'Name',
+                                prefixIcon: Icon(Icons.person),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your name';
+                                }
+                                return null;
+                              },
+                            ),
+                          if (!isLoginMode) SizedBox(height: 20),
+                          TextFormField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              prefixIcon: Icon(Icons.email),
+                              border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(30),
                               ),
                             ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty || !RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                                return 'Please enter a valid email';
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(height: 20),
+                          TextFormField(
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              prefixIcon: Icon(Icons.lock),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                            obscureText: true,
+                            validator: (value) {
+                              if (value == null || value.length < 6) {
+                                return 'Password must be at least 6 characters';
+                              }
+                              return null;
+                            },
+                          ),
+                          SizedBox(height: 20),
+                          if (errorMessage.isNotEmpty)
+                            Text(
+                              errorMessage,
+                              style: TextStyle(color: Colors.red, fontSize: 16),
+                            ),
+                          SizedBox(height: 10),
+                          isLoading
+                              ? CircularProgressIndicator()
+                              : ElevatedButton(
+                                  onPressed: () {
+                                    if (isLoginMode) {
+                                      login();
+                                    } else {
+                                      register();
+                                    }
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Color(0xFF446496),
+                                    padding: EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30),
+                                    ),
+                                  ),
+                                  child: Text(
+                                    isLoginMode ? 'Login' : 'Register',
+                                    style: TextStyle(color: Colors.white, fontSize: 18),
+                                  ),
+                                ),
+                          SizedBox(height: 20),
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                isLoginMode = !isLoginMode;
+                              });
+                            },
                             child: Text(
-                              isLoginMode ? 'Login' : 'Register',
-                              style: TextStyle(color: Colors.white, fontSize: 18),
+                              isLoginMode
+                                  ? "Don't have an account? Register here"
+                                  : "Already have an account? Login here",
+                              style: TextStyle(color: Color(0xFF446496), fontSize: 16),
                             ),
                           ),
-                    SizedBox(height: 20),
-                    GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          isLoginMode = !isLoginMode;
-                        });
-                      },
-                      child: Text(
-                        isLoginMode
-                            ? "Belum punya akun? Daftar di sini"
-                            : "Sudah punya akun? Login di sini",
-                        style: TextStyle(color: Color(0xFF446496), fontSize: 16),
+                        ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
